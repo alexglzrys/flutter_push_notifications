b05c2926f17f677048ef226bb0abce835008865b
Alejandro González Reyes <alexglzrys@gmail.com>
2024-03-23 14:15:44 -0600

    escuchar notificaciones

--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -7,6 +7,22 @@ class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
 
+  // Controladores de notificación
+  // ? Podemos reaccionar de forma diferente a los mensajes de notificación, según el estado actual de la aplicación en el dispositivo
+  static Future _onBackgroundMessageHandler(RemoteMessage message) async {
+    print(
+        'En segundo plano - _onBackgroundMessageHandler ${message.messageId}');
+  }
+
+  static Future _onMessageHandler(RemoteMessage message) async {
+    print('En uso - _onMessageHandler ${message.messageId}');
+  }
+
+  static Future _onMessageOpenedAppHandler(RemoteMessage message) async {
+    print(
+        'Aplicación cerrada - _onMessageOpenedAppHandler ${message.messageId}');
+  }
+
   static Future initializeApp() async {
     // inicializar la aplicaciòn de Firebase en la aplicación
     await Firebase.initializeApp(
@@ -14,5 +30,13 @@ class PushNotificationService {
     // Obtener el token asignado al dispositivo (este token es único y se asigna cuando la aplicación se instala en el dispositivo físico)
     token = await messaging.getToken();
     print('Token: $token');
+
+    // Registrar controladores de notificación
+    // * La aplicación está abierta pero en segundo plano (minimizada)
+    FirebaseMessaging.onBackgroundMessage(_onBackgroundMessageHandler);
+    // * La aplicación esta abierta,a la vista y en uso
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+    // * La aplicación esta cerrada o no se esta ejecutando, o el dispositivo se encuentra bloqueado
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenedAppHandler);
   }
 }
