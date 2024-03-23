ec0b57cc4174aae97b1ba1c84ef1fb2178f1f3c3
Alejandro González Reyes <alexglzrys@gmail.com>
2024-03-23 15:36:25 -0600

    reaccionar cuando recibimos una notificación

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,25 @@ void main() async {
   runApp(const PushNotificationApp());
 }
 
-class PushNotificationApp extends StatelessWidget {
+class PushNotificationApp extends StatefulWidget {
   const PushNotificationApp({super.key});
+
+  @override
+  State<PushNotificationApp> createState() => _PushNotificationAppState();
+}
+
+class _PushNotificationAppState extends State<PushNotificationApp> {
+  @override
+  void initState() {
+    // Suscribirse o escuchar la emisión de eventos que se transmiten en el controlador de flujo messageStream al momento que se inicialice mi aplicación
+    // ! La regla de las suscripciones establece que debe existir una cancelación cuando la aplicación termine, sin embargo, las notificaciones push deberían seguir llegando aunque la aplicación este terminada. Por ello no se cancela la suscripción al momento de destruir el app
+    PushNotificationService.messageStream.listen((message) {
+      // Dependiendo el código aquí declarado, nos permitirá reaccionar de forma diferente cuando se recibe una notificación
+      print('PushNotificationApp: $message');
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:push_notifications_flutter_app/firebase_options.dart';
@@ -6,21 +8,41 @@ class PushNotificationService {
   // Obtener la instancia de Firebase Cloud Messaging
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  // Crear un contriolador de flujo que emite datos de tipo String
+  // ? Quienes se suscriba a este flujo de información, puede recibir eventos (datos) de forma simultanea y reaccionar de forma diferente ante este suceso
+  // Se establece la base para la creación de un flujo de eventos que puede transmitir datos de tipo String a múltiples partes de la aplicación, permitiendo así la comunicación asíncrona entre componentes
+  static StreamController<String> _messageStream = StreamController.broadcast();
+
+  // Getters
+  // Definir un getter estático para tener acceso al flujo de datos que gestiona el controlador de flujo
+  static Stream<String> get messageStream => _messageStream.stream;
 
   // Controladores de notificación
   // ? Podemos reaccionar de forma diferente a los mensajes de notificación, según el estado actual de la aplicación en el dispositivo
   static Future _onBackgroundMessageHandler(RemoteMessage message) async {
     print(
         'En segundo plano - _onBackgroundMessageHandler ${message.messageId}');
+    // Cada notificación puede contener una sería de datos adicionales mejor conocido como carga útil
+    print(message.data);
+    // Enviar un nuevo evento al controlador de flujo, agregando el texto que viene adjunto en la notificación de Firebase Cloud Messaging
+    // ? Cualquier parte del código que este escuchando a messageStream, recibirá el mensaje cuando sea enviado. Esta es una forma de enviar datos a través de un flujo de eventos asíncrono en Flutter
+    _messageStream
+        .add(message.notification!.body ?? 'Sin texto en la notificación');
   }
 
   static Future _onMessageHandler(RemoteMessage message) async {
     print('En uso - _onMessageHandler ${message.messageId}');
+    print(message.data);
+    _messageStream
+        .add(message.notification!.body ?? 'Sin texto en la notificación');
   }
 
   static Future _onMessageOpenedAppHandler(RemoteMessage message) async {
     print(
         'Aplicación cerrada - _onMessageOpenedAppHandler ${message.messageId}');
+    print(message.data);
+    _messageStream
+        .add(message.notification!.body ?? 'Sin texto en la notificación');
   }
 
   static Future initializeApp() async {
